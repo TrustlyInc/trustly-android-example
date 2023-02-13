8be2c150c12f314cbaec9168aabf2e7329454bd4
Andre Guedes <andre.guedes@trustly.com>
2023-02-13 14:38:33 -0300

    Some establish data changes

---
+++ PayWithMyBankSDKDemoAndroid/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
+X
+(gradle/wrapper/gradle-wrapper.properties,f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+@
+local.properties,0/7/0712df971a99ac4d2fccb8e0fb19f377f3374cca
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
--- PayWithMyBankSDKDemoAndroid/app/src/main/java/net/trustly/paywithmybanksdkdemoandroid/EstablishData.kt
+++ PayWithMyBankSDKDemoAndroid/app/src/main/java/net/trustly/paywithmybanksdkdemoandroid/EstablishData.kt
@@ -4,22 +4,13 @@ object EstablishData {
 
     fun getEstablishDataValues(): Map<String, String> {
         val establishDataValues: MutableMap<String, String> = HashMap()
-        establishDataValues["accessId"] = "A48B73F694C4C8EE6307"
-        establishDataValues["requestSignature"] = "clYlSWb565B3JGUExdXrK5tyTqM="
-        establishDataValues["merchantId"] = "1009542823"
+        establishDataValues["accessId"] = "YOUR_ACCESS_ID"
+        establishDataValues["requestSignature"] = "YOUR_REQUEST_SIGNATURE"
+        establishDataValues["merchantId"] = "YOUR_MERCHANT_ID"
         establishDataValues["description"] = "Android SDK Demo"
         establishDataValues["currency"] = "USD"
-        establishDataValues["amount"] = "0.00"
-        establishDataValues["merchantReference"] = "3D51F3A42EFE499A"
+        establishDataValues["merchantReference"] = "MERCHANT_REFERENCE"
         establishDataValues["paymentType"] = "Retrieval"
-        establishDataValues["customer.name"] = "Freddie Mercury"
-        establishDataValues["customer.address.address1"] = "96725 Champlin Shoal"
-        establishDataValues["customer.address.city"] = "London"
-        establishDataValues["customer.address.state"] = "London"
-        establishDataValues["customer.address.zip"] = "DZ03 1WN"
-        establishDataValues["customer.address.country"] = "US"
-        establishDataValues["customer.phone"] = "+44 123456789"
-        establishDataValues["customer.email"] = "freddie.mercury@email.com"
         establishDataValues["metadata.urlScheme"] = "sdkdemo://"
         establishDataValues["env"] = "sandbox"
         return establishDataValues
