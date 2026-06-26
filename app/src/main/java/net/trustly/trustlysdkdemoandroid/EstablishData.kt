3eeb36de3604ca290a21decfb2598377ce82bd32
Andre Guedes <andre.guedes@trustly.com>
2026-06-26 09:45:12 -0300

    Updating trustly sdk to v4.3.0

--- app/src/main/java/net/trustly/trustlysdkdemoandroid/EstablishData.kt
+++ app/src/main/java/net/trustly/trustlysdkdemoandroid/EstablishData.kt
@@ -6,9 +6,9 @@ object EstablishData {
 
     fun getEstablishDataValues(): Map<String, String> {
         val establishDataValues: MutableMap<String, String> = HashMap()
-        establishDataValues["accessId"] = "TSwGyK52Mnpt5b8C"
+        establishDataValues["accessId"] = "YOUR_ACCESS_ID"
 //        establishDataValues["requestSignature"] = "YOUR_REQUEST_SIGNATURE"
-        establishDataValues["merchantId"] = "1127"
+        establishDataValues["merchantId"] = "YOUR_MERCHANT_ID"
         establishDataValues["description"] = "Android SDK Demo transaction"
         establishDataValues["currency"] = "USD"
         establishDataValues["merchantReference"] = "some_unique_transaction_identifier"
